f90c7bff8a5f14a09ee5cfec6ffff58a507e0b3f
Krisztian Horvath <keyki.kk@gmail.com>
2012-12-11 17:39:21 +0100

    jasper report fix

--- src/main/java/com/ajax/reverse/jasper/JasperReport.java
+++ src/main/java/com/ajax/reverse/jasper/JasperReport.java
@@ -7,36 +7,44 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JasperPrint;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.jasperreports.JasperReportsPdfView;
 
-import com.ajax.reverse.service.ChannelService;
-import com.ajax.reverse.service.MessageService;
+import com.ajax.reverse.domain.Channel;
+import com.ajax.reverse.service.JasperDatasourceService;
 
 @Component
 public class JasperReport extends JasperReportsPdfView {
 
     @Autowired
-    private ChannelService channelService;
-    @Autowired
-    private MessageService messageService;
-    private JRDataSource jRDataSource;
+    private JasperDatasourceService jasperDatasourceService;
 
     @Override
     protected JasperPrint fillReport(Map<String, Object> model) throws Exception {
-        model.put("datasource", jRDataSource);
+        String channelName = null;
+        Object channelObject = model.get("channel");
+        if (channelObject != null) {
+            if (channelObject instanceof Channel) {
+                channelName = ((Channel) channelObject).getName();
+            } else {
+                channelName = (String) channelObject;
+            }
+            JRDataSource jRDataSource = jasperDatasourceService.getDataSourceForChannel(channelName);
+            model.put("datasource", jRDataSource);
+        }
         return super.fillReport(model);
     }
 
     @Override
     protected void renderMergedOutputModel(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws Exception {
         super.renderMergedOutputModel(model, request, response);
-        String url = request.getRequestURL().toString();
-        String channel = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
-        jRDataSource = new JRBeanCollectionDataSource(messageService.findMessagesByChannel(channelService.findByName(channel), 100, 0));
+        if (!model.containsKey("channel")) {
+            String url = request.getRequestURL().toString();
+            String channel = url.substring(url.lastIndexOf("/") + 1, url.lastIndexOf("."));
+            model.put("channel", channel);
+        }
     }
 
 }
---
+++ src/main/java/com/ajax/reverse/service/JasperDatasourceService.java
@@ -0,0 +1,20 @@
+package com.ajax.reverse.service;
+
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JasperDatasourceService {
+
+    @Autowired
+    private ChannelService channelService;
+    @Autowired
+    private MessageService messageService;
+
+    public JRDataSource getDataSourceForChannel(String channel) {
+        return new JRBeanCollectionDataSource(messageService.findMessagesByChannel(channelService.findByName(channel), 100, 0));
+    }
+}
